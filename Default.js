34bfe631cf287c3139cbaea35fc305488ec52406
Pippohubplace <134394282+Pippohubplace@users.noreply.github.com>
2023-05-23 13:38:15 +0100

    Add files via upload

---
+++ Default.js
@@ -0,0 +1,36 @@
+let cautionbody=document.getElementById("cautionbody")
+
+let cautiontest=document.getElementById("cautiontext")
+
+function caution(){cautionbody.style.border="solid 1px red";cautiontest.innerHTML="Pi codes are  10digits";cautiontest.style.color="red"}
+
+function Caution2(){cautionbody.style.border="solid 1px red";cautiontest.innerHTML="!!! Sorry Pippohub doesn't recognize you";cautiontest.style.color="red"}
+
+function kaution3(){cautionbody.style.border="solid 1px red";cautiontest.innerHTML="input can't be left empty";cautiontest.style.color="red"}
+
+
+function verify(){let mytextlenght =document.getElementById("mytextlenght").value;
+var myreallength=mytextlenght.length;
+if (myreallength != "10") {caution()}
+else{getcertificate()}
+
+
+
+
+
+function getcertificate() {let piCodes =mytextlenght;
+
+switch(piCodes)
+{
+//.......!!!!!!!! Brand Name            ( NEUSITE [1])!!!!!............
+
+case "I56-EH5-17":
+document.open();document.write('<html><head><meta name="viewport" content="width=device-width,initial-scale=1.0"></head><body style="margin: 0px;background-image: linear-gradient(to bottom right,#463D5F,#080709);"><header style="height: 80px;margin: 10px 20px;background: #332641;border-radius: 20px;"><img src="./icons/logo.png" style="height: 60px;margin-top: 7px;"></header><section><h3 style="color: white;margin-left: 10px;font-family:serif"> Verification...</h3></section><section style="margin: 10px;"><center><div style="max-width: 550px;height: 720px;background: rgb(235, 233, 233);box-shadow: 0px 10px 10px black;border-radius: 0px;"><div style="height: 40px;background-color: #463D5F;"></div><div style="height: 130px;width: 130px;background-color:rgb(218, 215, 215)  ;margin-top: 20px;border-radius: 5px;float: left;margin-left: 10px;"></div><div><div style="height:40px;width:50%;background-color:rgb(218, 215, 215)  ;margin-top: 20px;border-radius: 5px;float: left;margin-left: 10px;"></div><div style="height:40px;width:50%;background-color:rgb(218, 215, 215)  ;margin-top: 20px;border-radius: 5px;float: left;margin-left: 10px;"></div><div style="height:40px;width:50%;background-color:rgb(218, 215, 215)  ;margin-top: 20px;border-radius: 5px;float: left;margin-left: 10px;"></div></div><br><br><br><br><br><br><br><br><br><br><br><div style="height: 10px;background-color: #463D5F;"></div><h5>Softwares / Languages</h5><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div></div></center></section><br><br><br><br><footer><div style="background:;width:100px;margin:10px auto"><p style="color: rgb(179, 176, 176); margin-top: 200px;position: fixed;">Pippohub &copy; 2023</p></div></footer></body></html>');document.close();break;
+
+case "456-TH5-F7":
+
+document.open(); document.write('<html><head></head><body style="margin: 0px;background-image: linear-gradient(to bottom right,#463D5F,#080709);"><header style="height: 80px;margin: 10px 20px;background: #332641;border-radius: 20px;"><img src="./icons/logo.png" style="height: 60px;margin-top: 7px;"></header><section><h3 style="color: white;margin-left: 10px;font-family:serif"> Verification ( <span style="color:rgb(190, 52, 245);font-size:15px">successful !</span> )</h3></section><section style="margin: 10px;"><center><div style="max-width: 550px;height: 720px;background: rgb(235, 233, 233);box-shadow: 0px 10px 10px black;border-radius: 0px;"><div style="height: 40px;background-color: #463D5F;"></div><div style="height: 130px;width: 130px;background-color:rgb(218, 215, 215)  ;margin-top: 20px;border-radius: 5px;float: left;margin-left: 10px;"></div><div><div style="height:40px;width:50%;background-color:rgb(218, 215, 215)  ;margin-top: 20px;border-radius: 5px;float: left;margin-left: 10px;"></div><div style="height:40px;width:50%;background-color:rgb(218, 215, 215)  ;margin-top: 20px;border-radius: 5px;float: left;margin-left: 10px;"></div><div style="height:40px;width:50%;background-color:rgb(218, 215, 215)  ;margin-top: 20px;border-radius: 5px;float: left;margin-left: 10px;"></div></div><br><br><br><br><br><br><br><br><br><br><br><div style="height: 10px;background-color: #463D5F;"></div><h5>Softwares / Languages</h5><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div><div style="background-color: rgb(218, 215, 215) ;height: 70px;margin: 5px 5px;"></div></div></center></section><br><br><br><br><footer><div style="background:;width:100px;margin:10px auto"><p style="color: rgb(179, 176, 176); margin-top: 200px;position: fixed;">Pippohub &copy; 2023</p></div></footer></body></html>');document.close();break;
+default:Caution2()}
+}
+
+}
